f4c1733a8b1699e5f180781be48d59c110291f08
ASviat <ecrprague.asi@gmail.com>
2024-04-29 21:36:50 +0300

    Init Shop

---
+++ Shop/src/main/java/Item.java
@@ -0,0 +1,18 @@
+public class Item {
+    String name;
+    double price;
+
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "[" +
+                "Наименование: '" + name + '\'' +
+                ", Цена: " + price +
+                ']';
+    }
+}
